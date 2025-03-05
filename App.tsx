47780527735c0d9176711daebb0ae644d3625850
ath tripathi <athtripathi@gmail.com>
2025-03-06 00:05:16 +0530

    structure modified

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from "react-native";
 import './global.css';
-import LoginScreen from "pages/Login";
+import LoginScreen from "./pages/Login";
 export default function App() {
   return (
    <LoginScreen/>
