4a94366a9bec0e8f0ce56ab6a6c157ec4a2d2f79
ath tripathi <athtripathi@gmail.com>
2025-03-06 00:02:30 +0530

    signup added

--- App.tsx
+++ App.tsx
@@ -1,12 +1,8 @@
-import React from "react";
-import { StatusBar } from "react-native";
-import AppNavigator from "./navigation"; // Adjust the path based on your file structure
-
+import { View, Text } from "react-native";
+import './global.css';
+import LoginScreen from "pages/Login";
 export default function App() {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <AppNavigator />
-    </>
+   <LoginScreen/>
   );
 }
