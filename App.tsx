8ae321776dd0af18d4e8ef028854d0b1e3f9eee5
Sparsh <gsparsh17@gmail.com>
2025-03-10 20:44:42 +0530

    commit

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
 import { View, Text } from "react-native";
 import './global.css';
 import LoginScreen from "pages/Login";
+import HomeScreen from "pages/home_screen";
+import OnboardingScreen from "pages/onboarding/screen1";
+import NavigatorPage from "pages/navigation_screen";
 export default function App() {
   return (
    <LoginScreen/>
