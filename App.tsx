707011d7e69b98b328219bd857b123063aacef2a
ath tripathi <athtripathi@gmail.com>
2025-03-13 20:53:24 +0530

    map pages added and navigation flow 1 set done with comments

--- App.tsx
+++ App.tsx
@@ -14,6 +14,7 @@ import BookingLocationScreen from "./pages/CabBooking/chooseLocationBooking";
 import MapScreen from "./pages/CabBooking/booking";
 import RideAcceptedScreen from "./pages/CabBooking/rideAccepted";
 import SignupPage from "pages/SignUp/Signup";
+// new comment
 // Create Stack Navigator
 const Stack = createStackNavigator();
 
