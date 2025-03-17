cfc4380d7aa6a843cfedcc887fcf380a130cdc94
Akshit Agarwal <154016441+akshit397a@users.noreply.github.com>
2025-03-17 23:36:47 +0530

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,7 @@
-// const { getDefaultConfig } = require('expo/metro-config');
-// const { withNativeWind } = require('nativewind/metro');
+const { getDefaultConfig } = require('expo/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
-// // eslint-disable-next-line no-undef
-// const config = getDefaultConfig(__dirname);
+// eslint-disable-next-line no-undef
+const config = getDefaultConfig(__dirname);
 
-// module.exports = withNativeWind(config, { input: './global.css' });
-
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind");
-
-module.exports = withNativeWind(
-  (() => {
-    const config = getDefaultConfig(__dirname);
-    config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== "svg");
-    config.resolver.sourceExts.push("svg");
-    return config;
-  })(),
-  { input: "./global.css" }
-);
-
+module.exports = withNativeWind(config, { input: './global.css' });
